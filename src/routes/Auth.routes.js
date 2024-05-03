ad498fba070f39ea1641958d82c7171bbe70c352
fabri075 <juanfabri69@hotmail.com>
2024-05-03 14:53:29 -0300

    Pruebas cambios

--- src/routes/Auth.routes.js
+++ src/routes/Auth.routes.js
@@ -1,16 +1,16 @@
-const express = require('express')
-const router = express.Router()
-const AuthService = require('../services/AuthService')
+const express = require("express");
+const router = express.Router();
+const AuthService = require("../services/AuthService");
 
-router.post('/login', async (req, res) => {
-    try {
-        const { mail, password } = req.body
-        const email = { email: mail }
-        const token = await AuthService.login(email, password)
-        res.json({ token })
-    } catch (error) {
-        res.status(400).json({ error: error.message })
-    }
-})
+router.post("/login", async (req, res) => {
+  try {
+    const { mail, password } = req.body;
+    const email = { email: mail };
+    const token = await AuthService.login(email, password);
+    res.json({ token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
 
-module.exports = router
+module.exports = router;
