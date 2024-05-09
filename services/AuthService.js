6797e9b1db86b68e6c091ec1d6e3cf5b0f202534
AguComba <acomba@coopmorteros.coop>
2024-05-09 11:22:21 -0300

    feat: Se agrego funcionalidad al controlador de login

--- services/AuthService.js
+++ services/AuthService.js
@@ -1,7 +1,13 @@
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
-const { sequelize } = require('../database/MSSQL.database')
+const { sequelizeCoopm_v1 } = require('../database/MySQL.database')
 const UserDesarrollo = require('../models/userDesarrollo')
+const crypto = require('crypto');
+
+//Generacion de la firma del token
+const secret = crypto.randomBytes(32).toString('hex');
+
+console.log(secret);
 
 const signToken = (user) => {
     return jwt.sign(
@@ -11,12 +17,13 @@ const signToken = (user) => {
             iat: new Date().getTime(),
             exp: new Date().setDate(new Date().getDate() + 1)
         },
-        'Cspmdesarrollo03'
+        secret
     )
 }
 
 exports.login = async (email, password) => {
-    const user = await UserDesarrollo.findOne({ where: email })
+    const user = await UserDesarrollo.findOne({ where: { email: email } })
+    console.log(user.dataValues)
     if (!user) {
         throw new Error('El usuario no existe')
     }
@@ -31,10 +38,9 @@ exports.login = async (email, password) => {
 
 const testConection = async () => {
     try {
-        await sequelize.authenticate()
-        console.log('CONEXIÓN EXITOSA')
+        await sequelizeCoopm_v1.authenticate()
     } catch (error) {
-        console.error('ERROR DE MIERDACOOP:', error)
+        console.error('ERROR DE DATABASE:', error)
     }
 }
 
