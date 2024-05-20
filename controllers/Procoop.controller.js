24a41e866290c21178f5689e27c84c979677b984
AguComba <acomba@coopmorteros.coop>
2024-05-20 08:52:59 -0300

    fix: Modificacion de metodos de busqueda por DNI y CUIT
    - Pasaron a ser metodos privados utilizando la cookie de sesion

--- controllers/Procoop.controller.js
+++ controllers/Procoop.controller.js
@@ -3,62 +3,62 @@ const State = require('../models/state.js')
 const { ListCity, ListState, empresaPorCuit, personaPorDni } = require('../services/ProcoopService.js')
 
 async function searchByDNI(req, res) {
-  const { dni } = req.body
-  try {
-    const result = await personaPorDni(dni)
-    return res.status(200).json(result)
-  } catch (error) {
-    return res.status(400).json({ error, msj: error.messagge })
-  }
+	const { dni } = req.body
+	try {
+		const result = await personaPorDni(dni)
+		return res.status(200).json(result)
+	} catch (error) {
+		return res.status(400).json({ error, msj: error.messagge })
+	}
 }
 
 async function searchByCuit(req, res) {
-  const { cuit } = req.body
-  try {
-    const result = await empresaPorCuit(cuit)
-    return res.status(200).json(result)
-  } catch (error) {
-    console.log(error)
-    return res.json({ error, msj: 'error' })
-  }
+	const { cuit } = req.body
+	try {
+		const result = await empresaPorCuit(cuit)
+		return res.status(200).json(result)
+	} catch (error) {
+		console.log(error)
+		return res.json({ error, msj: 'error' })
+	}
 }
 
 async function migrationCity(req, res) {
-  try {
-    const listCities = await ListCity();
-    let citiesOfi = [];
-    if (listCities) {
-      citiesOfi = await listCities.map((item) => {
-        return { COD_LOC: item.COD_LOC, DES_LOC: item.DES_LOC, COD_POS: item.COD_POS, COD_PCI: item.COD_PCI };
-      });
-    }
-    const resultadd = await City.bulkCreate(citiesOfi);
-    return res.status(200).json(resultadd);
-  } catch (error) {
-    console.log(error);
-    return res.json({ error, msj: "error" });
-  }
+	try {
+		const listCities = await ListCity()
+		let citiesOfi = []
+		if (listCities) {
+			citiesOfi = await listCities.map((item) => {
+				return { COD_LOC: item.COD_LOC, DES_LOC: item.DES_LOC, COD_POS: item.COD_POS, COD_PCI: item.COD_PCI }
+			})
+		}
+		const resultadd = await City.bulkCreate(citiesOfi)
+		return res.status(200).json(resultadd)
+	} catch (error) {
+		console.log(error)
+		return res.json({ error, msj: 'error' })
+	}
 }
 async function migrationState(req, res) {
-  try {
-    const ListStates = await ListState();
-    let listStateOfi = [];
-    if (ListStates) {
-      listStateOfi = await ListStates.map((item) => {
-        return { COD_PRO: item.COD_PRO, DES_PRO: item.DES_PRO, COD_AFIP: item.COD_AFIP };
-      });
-    }
-    const resultadd = await State.bulkCreate(listStateOfi);
-    return res.status(200).json(resultadd);
-  } catch (error) {
-    console.log(error);
-    return res.json({ error, msj: "error" });
-  }
+	try {
+		const ListStates = await ListState()
+		let listStateOfi = []
+		if (ListStates) {
+			listStateOfi = await ListStates.map((item) => {
+				return { COD_PRO: item.COD_PRO, DES_PRO: item.DES_PRO, COD_AFIP: item.COD_AFIP }
+			})
+		}
+		const resultadd = await State.bulkCreate(listStateOfi)
+		return res.status(200).json(resultadd)
+	} catch (error) {
+		console.log(error)
+		return res.json({ error, msj: 'error' })
+	}
 }
 
 module.exports = {
-  searchByCuit,
-  searchByDNI,
-  migrationCity,
-  migrationState
+	searchByCuit,
+	searchByDNI,
+	migrationCity,
+	migrationState,
 }
--- routes/Private.routes.js
+++ routes/Private.routes.js
@@ -1,22 +1,23 @@
-const express = require('express')
-const router = express.Router()
-const { migrationCity, migrationState } = require('../controllers/Procoop.controller')
-const { migrationUser, dataUser, allfacturas } = require('../controllers/User.controller')
-const { verifyToken } = require('../middleware/Auth.middleware')
-const { newQuery, logout } = require('../controllers/Auth.controller')
+const express = require('express');
+const router = express.Router();
+const { migrationCity, migrationState, searchByCuit, searchByDNI } = require('../controllers/Procoop.controller');
+const { migrationUser, dataUser, allfacturas } = require('../controllers/User.controller');
+const { verifyToken } = require('../middleware/Auth.middleware');
+const { newQuery, logout } = require('../controllers/Auth.controller');
 
 router.get('/test', (req, res) => {
-    res.json({ message: 'Test route' })
-})
+	res.json({ message: 'Test route' });
+});
 
 // router.get('/getUser', verifyToken)
 // router.get('/newQuery', verifyToken, newQuery)
-router.get('/logout', verifyToken, logout)
-
+router.get('/logout', verifyToken, logout);
+router.post('/searchDni', verifyToken, searchByDNI);
+router.post('/searchCuit', verifyToken, searchByCuit);
 // router.get('/users', migrationUser)
-router.post('/dataUser', verifyToken, dataUser)
-router.get('/allfacturas', verifyToken, allfacturas)
+router.post('/dataUser', verifyToken, dataUser);
+router.get('/allfacturas', verifyToken, allfacturas);
 // router.get('/localidad', migrationCity)
 // router.get('/provincia', migrationState)
 
-module.exports = router
+module.exports = router;
