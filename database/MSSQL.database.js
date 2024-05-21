c6633e1153cd5c09678c10c007fb95cc46181e2c
AguComba <acomba@coopmorteros.coop>
2024-05-21 07:33:50 -0300

    fix!: Se corrigio error en procop Service y se actualizo la conexion a procoop

--- database/MSSQL.database.js
+++ database/MSSQL.database.js
@@ -1,27 +1,27 @@
-const { Sequelize } = require("sequelize");
-const configDb = require("../config/config.json");
+const { Sequelize } = require('sequelize')
+const configDb = require('../config/config.json')
 
-sequelize = new Sequelize(configDb.procoop.database, configDb.procoop.username, configDb.procoop.password, {
-  host: configDb.procoop.host,
-  port: configDb.procoop.port,
-  dialect: configDb.procoop.dialect,
-  dialectOptions: {
-    options: {
-      encrypt: false,
-    },
-  },
-});
+SequelizeMorteros = new Sequelize(configDb.procoop.database, configDb.procoop.username, configDb.procoop.password, {
+	host: configDb.procoop.host,
+	port: configDb.procoop.port,
+	dialect: configDb.procoop.dialect,
+	dialectOptions: {
+		options: {
+			encrypt: false,
+		},
+	},
+})
 const SequelizeOncativo = new Sequelize(configDb.procoopOncativo.database, configDb.procoopOncativo.username, configDb.procoopOncativo.password, {
-  host: configDb.procoopOncativo.host,
-  port: configDb.procoopOncativo.port,
-  dialect: configDb.procoopOncativo.dialect,
-  dialectOptions: {
-    options: {
-      encrypt: false,
-    },
-  },
-});
+	host: configDb.procoopOncativo.host,
+	port: configDb.procoopOncativo.port,
+	dialect: configDb.procoopOncativo.dialect,
+	dialectOptions: {
+		options: {
+			encrypt: false,
+		},
+	},
+})
 module.exports = {
-  sequelize,
-  SequelizeOncativo,
-};
+	SequelizeMorteros,
+	SequelizeOncativo,
+}
--- services/ProcoopService.js
+++ services/ProcoopService.js
@@ -1,7 +1,7 @@
-const { sequelize } = require('../database/MSSQL.database')
+const { SequelizeMorteros } = require('../database/MSSQL.database')
 const conexionProcoop = async () => {
 	try {
-		await sequelize.authenticate()
+		await SequelizeMorteros.authenticate()
 		console.log('CONEXIÓN EXITOSA')
 	} catch (error) {
 		console.error('ERROR DE MIERDACOOP:', error)
@@ -11,14 +11,15 @@ const conexionProcoop = async () => {
 const personaPorDni = async (dni) => {
 	try {
 		const query = `SELECT * FROM personas WHERE NUM_DNI = :dni AND TIP_PERSO = 1`
-		const result = await sequelize.query(query, {
+		const result = await SequelizeMorteros.query(query, {
 			replacements: { dni: dni },
-			type: sequelize.QueryTypes.SELECT,
+			type: SequelizeMorteros.QueryTypes.SELECT,
 		})
 		if (result.length === 0) {
 			// Retorno un objeto con un mensaje de error
 			return { error: 'No se encontró la persona' }
 		}
+		return result
 	} catch (error) {
 		console.error('ERROR DE PROCOOP:', error)
 		throw error.message
@@ -28,9 +29,9 @@ const personaPorDni = async (dni) => {
 const empresaPorCuit = async (cuit) => {
 	try {
 		const query = `SELECT * FROM personas WHERE NUM_DNI = :cuit AND TIP_PERSO = 2`
-		const result = await sequelize.query(query, {
+		const result = await SequelizeMorteros.query(query, {
 			replacements: { cuit: cuit },
-			type: sequelize.QueryTypes.SELECT,
+			type: SequelizeMorteros.QueryTypes.SELECT,
 		})
 		if (result.length === 0) {
 			return { error: 'No se encontró la empresa' }
@@ -44,9 +45,9 @@ const empresaPorCuit = async (cuit) => {
 const invoicesXsocio = async (id_procoop) => {
 	try {
 		const query = `SELECT * FROM facturas WHERE id_procoop = :id_procoop`
-		const result = await sequelize.query(query, {
+		const result = await SequelizeMorteros.query(query, {
 			replacements: { id_procoop: id_procoop },
-			type: sequelize.QueryTypes.SELECT,
+			type: SequelizeMorteros.QueryTypes.SELECT,
 		})
 		if (result.length === 0) {
 			return { error: 'No se encontraron facturas' }
@@ -60,9 +61,9 @@ const invoicesXsocio = async (id_procoop) => {
 const Persona_x_COD_SOC = async (numberCustomer) => {
 	try {
 		const query = `SELECT * FROM socios  WHERE cod_soc = :numberCustomer`
-		const result = await sequelize.query(query, {
+		const result = await SequelizeMorteros.query(query, {
 			replacements: { numberCustomer: numberCustomer },
-			type: sequelize.QueryTypes.SELECT,
+			type: SequelizeMorteros.QueryTypes.SELECT,
 		})
 		if (result.length === 0) {
 			// Retorno un objeto con un mensaje de error
@@ -70,7 +71,7 @@ const Persona_x_COD_SOC = async (numberCustomer) => {
 		}
 		const query2 = `SELECT * FROM personas WHERE COD_PER = ${result[0].cod_per}`
 		const result2 = await sequelize.query(query2, {
-			type: sequelize.QueryTypes.SELECT,
+			type: SequelizeMorteros.QueryTypes.SELECT,
 		})
 		if (result2.length === 0) {
 			// Retorno un objeto con un mensaje de error
@@ -85,8 +86,8 @@ const Persona_x_COD_SOC = async (numberCustomer) => {
 const ListCityProcoop = async () => {
 	try {
 		const query = `SELECT * FROM localida`
-		const result = await sequelize.query(query, {
-			type: sequelize.QueryTypes.SELECT,
+		const result = await SequelizeMorteros.query(query, {
+			type: SequelizeMorteros.QueryTypes.SELECT,
 		})
 		if (result.length === 0) {
 			// Retorno un objeto con un mensaje de error
@@ -101,8 +102,8 @@ const ListCityProcoop = async () => {
 const ListStateProcoop = async () => {
 	try {
 		const query = `SELECT * FROM PROVINC`
-		const result = await sequelize.query(query, {
-			type: sequelize.QueryTypes.SELECT,
+		const result = await SequelizeMorteros.query(query, {
+			type: SequelizeMorteros.QueryTypes.SELECT,
 		})
 		if (result.length === 0) {
 			// Retorno un objeto con un mensaje de error
@@ -126,9 +127,9 @@ const serviceCustomer = async (data) => {
                     INNER JOIN Datos_ServiciosXSuministro dss ON dss.cod_sum = ds.cod_sum
                     INNER JOIN Servicio s ON s.cod_ser = dss.cod_ser
                     WHERE ds.${data.type} = :number ORDER BY ds.cod_sum`
-		const result = await sequelize.query(query, {
+		const result = await SequelizeMorteros.query(query, {
 			replacements: { number: data.number },
-			type: sequelize.QueryTypes.SELECT,
+			type: SequelizeMorteros.QueryTypes.SELECT,
 		})
 		if (result.length === 0) {
 			return { error: 'No se encontró la persona' }
@@ -154,9 +155,9 @@ const consumoCustomer = async (data) => {
 		const searchSince = `${lastMonth}/01/${lastYear}`
 		const query = `SELECT cod_ser, cod_sum, cod_med, fec_act, consumo, periodo, cod_cat,facturado, est_act FROM cons_ser 
     WHERE cod_ser = :ser AND fec_act >= :since AND cod_sum = :account`
-		const result = await sequelize.query(query, {
+		const result = await SequelizeMorteros.query(query, {
 			replacements: { ser: data.ser, since: searchSince, account: data.account },
-			type: sequelize.QueryTypes.SELECT,
+			type: SequelizeMorteros.QueryTypes.SELECT,
 		})
 		if (result.length === 0) {
 			return { error: 'No se encontró la persona' }
@@ -176,9 +177,9 @@ const debtsCustomer = async (number, all = false) => {
                   INNER JOIN pr_mt_nueva_demo.dbo.facturas fa ON fa.id_fac = dd.Id_Fac 
                   WHERE dd.cod_soc = :number AND dd.FECHA  >= Dateadd(mm,-13,Getdate()) ${all ? '' : 'AND dd.SALDO != 0'} 
                   ORDER BY FECHA DESC`
-		const result = await sequelize.query(query, {
+		const result = await SequelizeMorteros.query(query, {
 			replacements: { number: number },
-			type: sequelize.QueryTypes.SELECT,
+			type: SequelizeMorteros.QueryTypes.SELECT,
 		})
 		if (result.length === 0) {
 			return { error: 'No se encontró la persona' }
@@ -193,9 +194,9 @@ const phoneCustomer = async (account) => {
 	try {
 		const query = `SELECT ID_SERSOC, COD_SUM, COD_SER, COD_CAT, [NUM_MED/NUMTEL]
                   FROM Datos_ServiciosXSuministro WHERE COD_SUM = :account AND cod_ser = 10 AND fec_baja IS NULL`
-		const result = await sequelize.query(query, {
+		const result = await SequelizeMorteros.query(query, {
 			replacements: { account: account },
-			type: sequelize.QueryTypes.SELECT,
+			type: SequelizeMorteros.QueryTypes.SELECT,
 		})
 		if (result.length === 0) {
 			return { error: 'No se encontró la persona' }
@@ -217,9 +218,9 @@ const adheridosSS = async (data) => {
                   LEFT JOIN	document d ON pe.tip_dni = d.cod_doc 
                   LEFT JOIN	servicio s ON ad.cod_ser = s.cod_ser
                   WHERE	ad.cod_sum = :account AND ad.cod_ser IN (:ser1, :ser2) AND fec_baj IS NULL`
-		const result = await sequelize.query(query, {
+		const result = await SequelizeMorteros.query(query, {
 			replacements: { account: data.account, ser1: data.ser[0], ser2: data.ser[1] },
-			type: sequelize.QueryTypes.SELECT,
+			type: SequelizeMorteros.QueryTypes.SELECT,
 		})
 		if (result.length === 0) {
 			return { error: 'No se encontró la persona' }
