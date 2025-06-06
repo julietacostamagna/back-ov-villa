1ed55f8536d8d98bb585b4f78cae717b59ae6249
agust1n <acomba@coopmorteros.coop>
2025-06-06 07:35:58 -0300

    comentario para hacer el action

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// comentario para hacer el build
 const express = require('express')
 const app = express()
 const cookieParser = require('cookie-parser')
@@ -19,6 +20,6 @@ app.use(privateRoutes)
 app.use('/images', express.static('images'))
 
 app.listen(4000, () => {
-	console.log('Server is running on port 4000')
-	console.log('http://localhost:4000')
+    console.log('Server is running on port 4000')
+    console.log('http://localhost:4000')
 })
